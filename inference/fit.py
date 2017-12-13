400459a4856840202e1d7926ee0c746205bc56df
robert vogel <rvogel@ucla.edu>
2017-12-13 09:40:29 -0500

    initial commit

---
+++ inference/fit.py
@@ -0,0 +1,96 @@
+
+import numpy as np
+from scipy.optimize import fmin
+
+# ===============================================================
+# Fit the logistic function to data
+# ===============================================================
+
+def logistic(reference_samples, target,
+        max_iter=5e4, max_fun=5e4, disp=False,
+        ko=False):
+    '''
+    Fit the logistic model to data.  The logistic model used here is functionally identical to 1 - Fermi-Dirac statistics.  For simplicity the parameters are referred to by their physical interpretation, i.e. chemical potential and inverse temperature.
+
+    Input:
+        reference_samples : numpy array of single cell measurements representing the distribution P(x)
+        target : numpy array representing
+            [prevalance, prevalence * <x|class label =1>]
+    note that <x|c=1> = \int_{\forall x} x P(x|c=-1) dx.
+
+    Returns:
+        numpy array = [inverse temperature, chemical potential]
+    '''
+    if ko is False:
+        ko = _get_initial_parameters(np.mean(reference_samples), target)
+    out = fmin(_chi, ko, args=(reference_samples, target),
+                maxiter=max_iter, maxfun=max_fun,
+                disp=disp, full_output=True)
+    if out[4] != 0:
+        print 'did not converge'
+    return out[0]
+
+# ===============================================================
+# Initial parameter guess
+# ===============================================================
+
+def _get_initial_parameters(ref_mu, t):
+    '''
+    Initial guess of parameters values.  Assumes that the chemical potential is between the mean of P(x|c=1) and P(x|c=0), and the inverse temperature is 2/Delta.  Where Delta is <x|1> - <x|0>.
+
+    Input:
+    ref_mu : the mean of the reference distribution
+    t : model targets, i.e. [inverse temperature, chemical potential]
+
+    Returns
+    numpy array of initial model parameters
+    '''
+    q1 = t[1] / t[0]
+    qo = (ref_mu - t[1]) / (1-t[0])
+    dq = q1 - qo
+    #return np.hstack([np.mean(2./dq), q1 - 0.5*dq])
+    k = [np.mean(2./dq), q1 - 0.5*dq]
+    return np.array([k[0]*k[1], k[0]])
+
+# ===============================================================
+# Logistic function
+# ===============================================================
+
+def logistic_function(k, x):
+    '''
+    Logistic model
+    Input:
+    k : python tuple, list, or numpy array of model parameters, i.e. [inverse temperature, chemical potential]
+    x : scalar or numpy array of values of feature x
+
+    Return:
+    numpy array of P(c=1|x)
+    '''
+    return (1 + np.exp(k[0] - k[1]*x))**-1
+
+# ===============================================================
+# Objective function
+# ===============================================================
+
+def _chi(k, ref_samples, targets):
+    '''
+    Objective function: minimization of sum squared errors
+    Input:
+    k : numpy array of model parameters, i.e. [inverse temp, chem potential]
+    ref_samples : numpy array of individual events from the reference distribution, e.g. single cell measurements of feature x
+    targets : numpy array of statistical targets compute from the response distribution
+
+    Return:
+    a scalar, sum square error
+    '''
+    # evaluate model for all cells in the reference distribution
+    fd = logistic_function(k, ref_samples)
+    # The model inferred targets
+    t = np.array([np.mean(fd), np.mean(fd * ref_samples)])
+    # make sure that targets is a numpy array
+    try:
+        epsilon = 1 - t/targets
+    except TypeError:
+        print 'Targets are in an unrecognized data type.  Data are transformed to numpy array.'
+        epsilon = 1 - t/np.array(targets)
+    return np.sum(epsilon**2)
